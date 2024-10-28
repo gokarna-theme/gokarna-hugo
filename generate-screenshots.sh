b6fea21df90f84253352277dfcebc8b1f4215e26
James Davidson <james@jamesdavidson.xyz>
2024-10-28 10:11:29 +0000

    feat: add naive build script
    container cp not yet working from script

---
+++ generate-screenshots.sh
@@ -0,0 +1,18 @@
+#! /usr/bin/env bash
+
+PLATFORM=podman
+
+main ()
+{
+    local tag='gokarna-screenshot.py-image'
+    local name='gokarna-screenshot.py-container'
+
+    "$PLATFORM" rm "$name"
+
+    "$PLATFORM" build --tag="$tag" -- .
+    "$PLATFORM" create --name="$name" -- "$tag"
+    "$PLATFORM" start "$name"
+    # "$PLATFORM" container cp "$name":/usr/src/app/images/ .
+}
+
+main
