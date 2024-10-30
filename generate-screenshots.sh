f11a7435181739e7a669b31147a09bfd7fe23227
James Davidson <james@jamesdavidson.xyz>
2024-10-30 18:56:16 +0000

    refactor(generate-screenshots): rewrite script
    - update containers (not images) with workdir files
    - force rebuild images when image already exists
    - fix cp from container to host

--- generate-screenshots.sh
+++ generate-screenshots.sh
@@ -10,18 +10,50 @@ set -eux
 #readonly PLATFORM=docker
 readonly PLATFORM=podman
 
-main ()
+readonly TAG='gokarna-screenshot.py-image'
+readonly NAME='gokarna-screenshot.py-container'
+
+run ()
 {
-    local tag='gokarna-screenshot.py-image'
-    local name='gokarna-screenshot.py-container'
-
-    # Remove existing container
-    "$PLATFORM" rm "$name"
-
-    "$PLATFORM" build --tag="$tag" -- .
-    "$PLATFORM" create --name="$name" -- "$tag"
-    "$PLATFORM" start "$name"
-    # "$PLATFORM" container cp "$name":/usr/src/app/images/ .
+    $PLATFORM rm $NAME
+    $PLATFORM run --name=$NAME -- $TAG
 }
 
-main
+build_or_update ()
+{
+    podman inspect --type=image $TAG 1>/dev/null
+    # Determine if $TAG exists by evaluating the exit code
+    local image_exists=$?
+
+    # If there is no existing image, or -f was given
+    if [ $image_exists -ne 0 ] || [ "$1" -eq 1 ]
+    then
+        $PLATFORM build --tag=$TAG -- .
+        run
+    elif [ $image_exists -eq 0 ]
+    then
+        # Update the files in the container
+        run
+        $PLATFORM cp . $NAME:/usr/src/app/
+    fi
+}
+
+main ()
+{
+    local force_rebuild=0
+
+    # If no arguments are given
+    if [ $# -eq 0 ]
+    then
+        return
+    elif [ "$1" = f ] || [ "$1" = -f ] || [ "$1" = --force ]
+    then
+            force_rebuild=1
+    fi
+
+    build_or_update $force_rebuild
+
+    $PLATFORM cp $NAME:/usr/src/app/images/ .
+}
+
+main "$@"
