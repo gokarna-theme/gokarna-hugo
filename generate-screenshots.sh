9d3e91147c5df43e619ab07006c0da553e70bf4a
James Davidson <james@jamesdavidson.xyz>
2024-10-30 20:25:06 +0000

    feat!: remove update facility

--- generate-screenshots.sh
+++ generate-screenshots.sh
@@ -4,24 +4,21 @@ set -eux
 
 # Build a Python container which runs screenshot.py
 
-FORCE_REBUILD=0
 PLATFORM='podman'
 
-while getopts fp:h flag
+while getopts p:h flag
 do
     case $flag in
-        f ) FORCE_REBUILD=1 ;;
         p ) PLATFORM=$OPTARG ;;
         \? | h | * )
-            printf '%b' 'generate-screenshots.sh [-f] [-p buildah|docker|podman]\n  -f: force image rebuild (skip check for existing image)\n  -p: platform binary (default: podman)\n' \
-                && exit 1 ;;
+            printf '%b' 'generate-screenshots.sh [-p buildah|docker|podman]\n  -p: platform binary (default: podman)\n' ;;
     esac
 done
 
 readonly TAG='gokarna-screenshot.py-image'
 readonly NAME='gokarna-screenshot.py-container'
 
-run ()
+main ()
 {
     result=$($PLATFORM ps --all --quiet --filter name=$NAME)
     if [ -n "$result" ]
@@ -29,31 +26,8 @@ run ()
         $PLATFORM rm $NAME
     fi
 
+    $PLATFORM build --tag=$TAG -- .
     $PLATFORM run --name=$NAME -- $TAG
-}
-
-build_or_update ()
-{
-    podman inspect --type=image $TAG 1>/dev/null
-    # Determine if $TAG exists by evaluating the exit code
-    local image_exists=$?
-
-    # If there is no existing image, or -f was given
-    if [ $image_exists -ne 0 ] || [ "$1" -eq 1 ]
-    then
-        $PLATFORM build --tag=$TAG -- .
-        run
-    elif [ $image_exists -eq 0 ]
-    then
-        # Update the files in the container
-        run
-        $PLATFORM cp . $NAME:/usr/src/app/
-    fi
-}
-
-main ()
-{
-    build_or_update "$FORCE_REBUILD"
     $PLATFORM cp $NAME:/usr/src/app/images/ .
 }
 
